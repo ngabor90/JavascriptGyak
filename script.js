dd2eac65ef01402a5c4809448c36918a7f56da82
ngabor90 <n.gabor90@gmail.com>
2023-11-26 17:22:57 +0100

    Feladat2 kiegeszites

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function KorKerTer(r) {
 console.log(VanENegativ([5, 12, 0, 23, -1, 54]));
 console.log(VanENegativ([5, 12, 0, 23, 54]));
 console.log(KorKerTer(3));
+function MinAuto(autok) {
+    var MinAuto = autok[0];
+    for (var i = 1; i < autok.length; i++) {
+        if (autok[i].hengerurtartalom < MinAuto.hengerurtartalom) {
+            MinAuto = autok[i];
+        }
+    }
+    return MinAuto;
+}
+function BenzinesDb(autok) {
+    var db = 0;
+    for (var i = 0; i < autok.length; i++) {
+        if (autok[i].benzinesE) {
+            db++;
+        }
+    }
+    return db;
+}
+var a1 = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 1200,
+    benzinesE: true
+};
+var a2 = {
+    gyarto: "Volswagen",
+    tipus: "Polo",
+    hengerurtartalom: 1400,
+    benzinesE: false
+};
+var a3 = {
+    gyarto: "Skoda",
+    tipus: "Octavia",
+    hengerurtartalom: 1600,
+    benzinesE: true
+};
+var autok = [a1, a2, a3];
+console.log(MinAuto(autok));
+console.log(BenzinesDb(autok));
--- script.ts
+++ script.ts
@@ -24,3 +24,52 @@ interface Auto{
     hengerurtartalom:number;
     benzinesE:boolean;
 }
+
+function MinAuto(autok:Auto[]):Auto{
+    var MinAuto:Auto = autok[0];
+
+    for(var i:number = 1; i < autok.length; i++){
+        if(autok[i].hengerurtartalom < MinAuto.hengerurtartalom){
+            MinAuto = autok[i];
+        }
+    }
+
+    return MinAuto;
+}
+
+function BenzinesDb(autok:Auto[]):number{
+    var db:number = 0;
+
+    for(var i:number = 0; i < autok.length; i++){
+        if(autok[i].benzinesE){
+            db++;
+        }
+    }
+    return db; 
+}
+
+var a1:Auto = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 1200,
+    benzinesE: true
+};
+
+var a2:Auto = {
+    gyarto: "Volswagen",
+    tipus: "Polo",
+    hengerurtartalom: 1400,
+    benzinesE: false
+};
+
+var a3:Auto = {
+    gyarto: "Skoda",
+    tipus: "Octavia",
+    hengerurtartalom: 1600,
+    benzinesE: true
+};
+
+var autok:Auto[] = [a1, a2, a3];
+
+console.log(MinAuto(autok));
+console.log(BenzinesDb(autok));
